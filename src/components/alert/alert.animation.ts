1dc86a7727172bcef421be922c321786b4343b74
drelocatelli <andressalocatelli@hotmail.com>
2023-03-28 13:05:54 -0300

    new alert style

--- src/components/alert/alert.animation.ts
+++ src/components/alert/alert.animation.ts
@@ -3,8 +3,8 @@ import {gsap} from 'gsap';
 export default function AlertFx() {
     const timeline = gsap.timeline({paused: false});
     timeline.to('.alert', {duration: 0, x: '300px'})
-    timeline.to('.alert', {opacity: 1, pointerEvents: 'all'})
-        timeline.to('.alert', {duration: .6, ease: 'back', x: '0'})
+    timeline.to('.alert', {opacity: .8, pointerEvents: 'all'})
+        timeline.to('.alert', {duration: .4, ease: 'back', x: '0'})
 }
 
 export function AlertCloseFx() {
