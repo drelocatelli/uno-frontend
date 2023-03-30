c1873ab81b9564f8f54e8939c506464780da8bfe
drelocatelli <andressalocatelli@hotmail.com>
2023-03-30 19:02:35 -0300

    adding pagination

--- src/components/lobby/roomCard.component.tsx
+++ src/components/lobby/roomCard.component.tsx
@@ -1,7 +1,7 @@
 function RoomCard(props: { id: string; title: string; user: string; count: string; disabled?: boolean }) {
     return (
         <label className="card-label" htmlFor={`${props.id}_radio`}>
-                    <input type="radio" id={`${props.id}_radio`} className="roomRadio" name="room" disabled={props.disabled ?? false} />
+            <input type="radio" id={`${props.id}_radio`} className="roomRadio" name="room" value={props.id} disabled={props.disabled ?? false} />
             <div id={props.id} className={`card ${props.disabled ? 'disabled' : ''}`}>
                 <b>{props.title}</b>
                 <div className="user">#{props.user}</div>
