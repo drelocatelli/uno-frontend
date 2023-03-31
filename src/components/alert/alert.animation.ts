9437d659cb7b052944701eccb122d080df890fda
drelocatelli <andressalocatelli@hotmail.com>
2023-03-31 13:55:39 -0300

    refactoring

--- src/components/alert/alert.animation.ts
+++ src/components/alert/alert.animation.ts
@@ -1,13 +1,13 @@
 import {gsap} from 'gsap';
 
 export default function AlertFx() {
-    const timeline = gsap.timeline({paused: false});
-    timeline.to('.alert', {duration: 0, x: '300px'})
-    timeline.to('.alert', {opacity: .8, pointerEvents: 'all'})
-        timeline.to('.alert', {duration: .4, ease: 'back', x: '0'})
+    // const timeline = gsap.timeline({paused: false});
+    // timeline.to('.alert', {duration: 0, x: '300px'})
+    // timeline.to('.alert', {opacity: .8, pointerEvents: 'all'})
+    //     timeline.to('.alert', {duration: .4, ease: 'back', x: '0'})
 }
 
 export function AlertCloseFx() {
-    const timeline = gsap.timeline({paused: false});
-    timeline.to('.alert', {duration: .4, ease: 'circ', pointerEvents: 'none', opacity: 0})
+    // const timeline = gsap.timeline({paused: false});
+    // timeline.to('.alert', {duration: .4, ease: 'circ', pointerEvents: 'none', opacity: 0})
 }
