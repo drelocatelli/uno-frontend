0feef66e896291064699eebc4e62a07ca261d3dd
drelocatelli <andressalocatelli@hotmail.com>
2023-04-01 17:23:46 -0300

    exclude Path to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from "node:url";
+
+const filesNeedToExclude = ["src/pluggables/Comp1.vue", "src/pluggables/Comp2.vue"];
+
+const filesPathToExclude = filesNeedToExclude.map((src) => {
+  return fileURLToPath(new URL(src, import.meta.url));
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +14,12 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      external: [
+        ...filesPathToExclude
+      ]
+    }
+  }
 })
