f966d325acbb3d86df00b88355d7dedbfb0217dd
drelocatelli <andressalocatelli@hotmail.com>
2023-04-01 17:13:10 -0300

    adding more tests

--- src/components/login/test/login.spec.tsx
+++ src/components/login/test/login.spec.tsx
@@ -1,14 +1,40 @@
-import {describe, expect, it, test} from 'vitest';
+import {describe, expect, it} from 'vitest';
 import {render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { TestComponent } from './testComponent';
 import Guest from '../guest.component';
 
-describe('render guest login', () => {
-    test('wait text appers on screen', async () => {
-        render(<TestComponent children={<Guest />} />);
+describe('guest login', () => {
+    const {container, debug} = render(<TestComponent children={<Guest />} />);
 
+    it('wait text appers on screen', () => {
         waitFor(() => {
             expect(screen.findByText('Escolha seu nick')).toBeInTheDocument()
         });
     })
+
+    it('fail when nickname is null', () => {
+        const btn = screen.findByText('Entrar');
+        waitFor(async () => {
+            expect(btn).toBeInTheDocument();
+            userEvent.click(await btn);
+            expect(screen.findByText('Preencha este campo.')).toBeInTheDocument();
+        })
+    });
+
+    it('try to get avatar', () => {
+        const btnRandomAvatar = container.getElementsByClassName('reload-icon').item(0);
+        waitFor(async () => {
+            expect(btnRandomAvatar).toBeInTheDocument();
+            if(btnRandomAvatar) {
+                debug();
+                userEvent.click(await btnRandomAvatar);
+                debug();
+                // expect()
+            } else {
+                throw new Error('Cannot find this button');
+            }
+            
+        });
+    });
 })
