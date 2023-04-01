f86e0f4970009ccab1a783e8f3a789a96be4716e
drelocatelli <andressalocatelli@hotmail.com>
2023-04-01 17:36:01 -0300

    adding e2e testing module

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath } from "node:url";
 
-const filesNeedToExclude = ["src/pluggables/Comp1.vue", "src/pluggables/Comp2.vue"];
+const filesNeedToExclude = ["testing/**/*"];
 
 const filesPathToExclude = filesNeedToExclude.map((src) => {
   return fileURLToPath(new URL(src, import.meta.url));
