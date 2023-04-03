8b395c5f80bcf0620545cb3090bb084d2c3b5df1
drelocatelli <andressalocatelli@hotmail.com>
2023-04-03 11:33:02 -0300

    change logo

--- testing/src/index.ts
+++ testing/src/index.ts
@@ -1,18 +1,21 @@
-import { Page } from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
 import AutomationTestSetup from '../bootstrap/AutomationTestSetup';
 import LoginTest from './tests/login.spec';
+import ResponsivityTest from './tests/responsivity.spec';
 
 class Test extends AutomationTestSetup {
     tests = {
         login: (page: Page) => new LoginTest(page),
+        responsivity: (page: Page, browser: Browser) => new ResponsivityTest(page, browser),
     };
 
     constructor() {
         super();
         this.initializer('.container').then(async ({ page, browser }) => {
             while (true) {
+                // select test order
                 await this.runTests([
-                    this.tests.login(page)
+                    this.tests.login(page),
                 ]);
                 await this.readLine('Select [Enter]: ');
             }
--- testing/src/tests/responsivity.spec.ts
+++ testing/src/tests/responsivity.spec.ts
@@ -2,38 +2,45 @@ import { Browser, Page } from "puppeteer";
 import AutomationTestSetup from "../../bootstrap/AutomationTestSetup";
 
 class ResponsivityTest extends AutomationTestSetup {
+    constructor(page: Page, browser: Browser) {
+        super();
+        this.assert(page, browser);
+    }
+    
     init() {
         this.initializer().then(async ({page, browser}) => {
             await page.waitForSelector('.container');
-            await this.takeSnapshot(page, browser);    
+            await this.assert(page, browser);    
         })
     }
-
-    async takeSnapshot(page: Page, browser: Browser) {
-        await page.setViewport({isMobile: true, width: 320, height: 480});
-        setTimeout(async () => {
-            await page.screenshot({
-                path: './src/assets/mobile.png'
-            })
-        }, 2000);
-        await page.setViewport({isMobile: true, width: 768, height: 1024});
-        setTimeout(async () => {
-            await page.screenshot({
-                path: './src/assets/tablet.png'
-            })
-        }, 2000);
-        await page.setViewport({isMobile: false, width: 0, height: 0});
-        setTimeout(async () => {
-            await page.screenshot({
-                path: './src/assets/desktop.png'
-            });
-        }, 2000);
-        browser.close();
+    
+    async assert(page: Page, browser: Browser) {
+        const question = await this.readLine('Take snapshot? [Y|n]:') as ('y' | 'n');
+        console.log('question', question)
+        if(question == 'y') {
+            await page.setViewport({isMobile: true, width: 320, height: 480});
+            setTimeout(async () => {
+                await page.screenshot({
+                    path: './src/assets/mobile.png'
+                })
+            }, 2000);
+            await page.setViewport({isMobile: true, width: 768, height: 1024});
+            setTimeout(async () => {
+                await page.screenshot({
+                    path: './src/assets/tablet.png'
+                })
+            }, 2000);
+            await page.setViewport({isMobile: false, width: 0, height: 0});
+            setTimeout(async () => {
+                await page.screenshot({
+                    path: './src/assets/desktop.png'
+                });
+            }, 2000);
+            browser.close();
+        } 
     }
 
 
 }
-
-new ResponsivityTest().init();
 
 export default ResponsivityTest;
